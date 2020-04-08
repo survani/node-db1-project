6f879e6e8a77a714334506e626fc98b8f4e91e42
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-04-08 09:05:15 -0700

    Small fixes to format of queries.sql file

--- queries.sql
+++ queries.sql
@@ -22,19 +22,19 @@ LIMIT 10
 -- Find all customers that live in London, Madrid, or Brazil
 
 SELECT *
-FROM Customers
+FROM [Customers]
 WHERE City='London' OR City='Madrid' OR Country='Brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
-INSERT INTO Customers
+INSERT INTO [Customers]
     (CustomersName, ContactName, Address, City, PostalCode,Country)
 VALUES
     ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
-UPDATE Customers 
+UPDATE [Customers] 
 SET PostalCode = '111222'
 WHERE CustomerID = 92
 
